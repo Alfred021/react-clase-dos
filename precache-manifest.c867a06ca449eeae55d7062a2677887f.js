31910b6916316295511a30e68bc701fcb617fe97
Alfred021 <alfredomedina10@hotmail.com>
2020-10-10 21:27:05 -0300

    Updates

---
+++ precache-manifest.c867a06ca449eeae55d7062a2677887f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04f03cbe1d4156fe0555874fd0bb0b2",
+    "url": "/react-clase-dos/tree/Memotest/index.html"
+  },
+  {
+    "revision": "732941b953596c4ffbc5",
+    "url": "/react-clase-dos/tree/Memotest/static/css/main.12a07c95.chunk.css"
+  },
+  {
+    "revision": "c9d97a89e785536870d0",
+    "url": "/react-clase-dos/tree/Memotest/static/js/2.54762264.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-clase-dos/tree/Memotest/static/js/2.54762264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "732941b953596c4ffbc5",
+    "url": "/react-clase-dos/tree/Memotest/static/js/main.ae0743a0.chunk.js"
+  },
+  {
+    "revision": "3c80cc011a50b8424e5f",
+    "url": "/react-clase-dos/tree/Memotest/static/js/runtime-main.60b3f69e.js"
+  }
+]);
